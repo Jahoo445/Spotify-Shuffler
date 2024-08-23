7cc80387ad8c0bf4224ff0435d62f2663c9bfb6b
Janis Hofstetter <hoj@bica.ch>
2024-08-23 15:06:52 +0200

    chore: Update project files and dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ArtistComponent } from './artist/artist.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ArtistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/artist/artist.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Artist } from './artists.type';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-artist',
+  standalone: true,
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
+  templateUrl: './artist.component.html',
+  styleUrl: './artist.component.scss'
+})
+export class ArtistComponent {
+  selectedArtist: Artist = { id: '', artistName: '' };
+
+  artists: Artist[] = [
+    { id: '10fua9lLREs5JISPcCyyJn', artistName: 'lcone' },
+    { id: '0JBdTCGs111JKKYfLqOEBa', artistName: 'Shirin David' },
+    { id: '7wkPBPwF9oOZJ8lEbQjIVt', artistName: 'Mani Matter' },
+    { id: '1DxUdl4z0N2hLqU7U6yqwc', artistName: 'money Boy' },
+    { id: '3TVXtAsR1Inumwj472S9r4', artistName: 'Drake' },
+  ]
+
+  async getArtistsAlbum(artistId: string): Promise<void> {
+    const url = `https://api.spotify.com/v1/artists/${artistId}/albums`;
+
+    const token = 'BQCYNgL8ILuJnlnJ8feoQ00DNdDwTZq6iyuHg3SbCwpNqOmeXZUFlDrjTZa5qTZVTut8E_TTUC7uLT7Pkj7XhqTct2V4vxJkJp1eRojXCjOXV5yrp8A'
+
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token
+      }
+    }
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+
+  }
+}
+
+
+
