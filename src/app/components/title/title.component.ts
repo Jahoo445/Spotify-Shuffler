b31f75e1cbaf4cf2832cfb3cc7447ed427ebb472
Janis Hofstetter <hoj@bica.ch>
2024-10-28 10:06:48 +0100

    fix

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, ElementRef, AfterViewChecked, Renderer2 } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewChecked, Renderer2, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-title',
@@ -9,18 +10,38 @@ import { Component, Input, ElementRef, AfterViewChecked, Renderer2 } from '@angu
 export class TitleComponent implements AfterViewChecked {
   @Input() text: string = '';
 
-  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
 
   ngAfterViewChecked() {
-    this.adjustFontSize();
+    if (isPlatformBrowser(this.platformId)) {
+      this.adjustFontSize();
+    }
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.adjustFontSize();
+    }
   }
 
   private adjustFontSize() {
+    if (!isPlatformBrowser(this.platformId)) return;
+
     const titleElement = this.el.nativeElement.querySelector('.title');
+    const screenWidth = window.innerWidth;
+
+    console.log(screenWidth);
 
     console.log(this.text.length);
 
-    if (this.text.length >= 15) {
+    if (screenWidth < 445) {
+      this.renderer.setStyle(titleElement, 'font-size', '2.9rem');
+    } else if (this.text.length >= 15) {
       this.renderer.setStyle(titleElement, 'font-size', '3.5rem');
     } else {
       this.renderer.setStyle(titleElement, 'font-size', '4rem');
