a4d10e0010b2625886ce68db37152352d175f5fb
Jahoo445 <hoj@bica.ch>
2024-11-11 11:08:27 +0100

    refactor fetchSpotifyToken function to improve error handling and remove debug logs

--- netlify/functions/fetchSpotifyToken.js
+++ netlify/functions/fetchSpotifyToken.js
@@ -1,33 +1,25 @@
 exports.handler = async function (event, context) {
     try {
-        console.log("Fetching Spotify token...");
-        console.log("Client ID:", process.env.SPOTIFY_CLIENT_ID);
-        console.log("Client Secret:", process.env.SPOTIFY_CLIENT_SECRET);
-
         const response = await fetch('https://accounts.spotify.com/api/token', {
             method: 'POST',
             headers: {
                 'Authorization': 'Basic ' + Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64'),
-                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Type': 'application/x-www-form-urlencoded'
             },
-            body: new URLSearchParams({ 'grant_type': 'client_credentials' }),
+            body: new URLSearchParams({ 'grant_type': 'client_credentials' })
         });
-
         if (!response.ok) {
-            throw new Error(`Spotify API request failed with status ${response.status}`);
+            throw new Error('Failed to fetch token');
         }
-
         const data = await response.json();
-
         return {
             statusCode: 200,
             body: JSON.stringify({ accessToken: data.access_token }),
         };
     } catch (error) {
-        console.error("Error fetching Spotify token:", error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: "Failed to fetch token" }),
+            body: JSON.stringify({ error: error.message }),
         };
     }
-};
+}; 
